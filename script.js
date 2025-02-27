8a3bf1c553c48e3b82da56ce59bb6558faa8fc6d
Akash Vishwakarma <107378176+akash2v@users.noreply.github.com>
2025-02-28 01:19:15 +0530

    update script to handle dark mode

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-// This script change mode of the page from light to dark and vice versa
+// This script changes the mode of the page from light to dark and vice versa
+let darkMode = localStorage.getItem("darkMode") || "false";
+if (darkMode === "true") {
+    let element = document.body;
+    element.classList.add("dark-mode");
+}
+const darkModeToggle = document.querySelector("#themeToggle");
 changeMode = () => {
     let element = document.body;
     element.classList.toggle("dark-mode");
+    darkMode = (darkMode === "true") ? "false" : "true";
+    localStorage.setItem("darkMode", darkMode);
 }
-
-document.getElementById("themeToggle").addEventListener("click", changeMode);
+darkModeToggle.addEventListener("click", changeMode);
